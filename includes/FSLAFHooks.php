570d027c9cd41f577afc1e95e8d0f5a14927a844
thomas-topway-it <topway.it@mail.com>
2025-05-20 02:25:01 +0400

    add vector-2022 cond

--- includes/FSLAFHooks.php
+++ includes/FSLAFHooks.php
@@ -3,9 +3,11 @@
 class FSLAFHooks {
 
 	public static function addModules( OutputPage $out, Skin $skin ) {
-		$out->addModules( [ 'ext.fslookandfeel.scripts' ] );
-		$out->addModuleStyles( [ 'ext.fslookandfeel.styles' ] );
-		return true;
+		if ( $GLOBALS['wgDefaultSkin'] === 'vector-2022' ) {
+			$out->addModules( [ 'ext.fslookandfeel.scripts' ] );
+			$out->addModuleStyles( [ 'ext.fslookandfeel.styles' ] );
+			return true;
+		}
 	}
 
 }
