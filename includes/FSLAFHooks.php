462919564abebf17d7c9336cdd3a3aaf7490249d
thomas-topway-it <topway.it@mail.com>
2025-05-20 19:55:33 +0400

    update injected header host and module names

--- includes/FSLAFHooks.php
+++ includes/FSLAFHooks.php
@@ -4,8 +4,8 @@ class FSLAFHooks {
 
 	public static function addModules( OutputPage $out, Skin $skin ) {
 		if ( $GLOBALS['wgDefaultSkin'] === 'vector-2022' ) {
-			$out->addModules( [ 'ext.fslookandfeel.scripts' ] );
-			$out->addModuleStyles( [ 'ext.fslookandfeel.styles' ] );
+			$out->addModules( [ 'ext.zionlookandfeel.scripts' ] );
+			$out->addModuleStyles( [ 'ext.zionlookandfeel.styles' ] );
 			return true;
 		}
 	}
--- resources/fslaf-injectedHeader.js
+++ resources/fslaf-injectedHeader.js
@@ -1,6 +1,11 @@
 ( function() {
+	var localHost = ( window.location.hostname === "localhost" ||
+		window.location.hostname === "127.0.0.1" ||
+		window.location.hostname === "[::1]" // IPv6 localhost
+	);
+
 	var injHF = document.createElement('script');
 	injHF.async = true;
-	injHF.src = 'https://familysearch.org/hf/hf.js';
+	injHF.src = 'https://' + ( !localHost ? window.location.host : 'familysearch.org' ) + '/hf/hf.js';
 	document.getElementsByTagName('head')[ 0 ].appendChild( injHF );
 } )();
