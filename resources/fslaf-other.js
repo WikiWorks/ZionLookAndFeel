1ee82b31685b848cbc3f50b81d4da47d926b8fb4
thomas-topway-it <topway.it@mail.com>
2025-06-01 03:58:32 +0400

    fix max-width, fix colums, fix buttons, hide section edit

--- resources/fslaf-other.js
+++ resources/fslaf-other.js
@@ -3,6 +3,7 @@
 		$('.vector-user-links').toggle( false );
 		$('.vector-menu-content').toggle( false );
 		$('#right-navigation').toggle( false );
+		$('.mw-editsection').toggle( false );
 	}
 	// $('.mw-logo').replaceWith($('#google_translate_element'));
 } )();
