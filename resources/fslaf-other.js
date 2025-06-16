ace392d2907fd966ecd18b7319a6282e25034b86
thomas-topway-it <topway.it@mail.com>
2025-06-17 02:09:45 +0400

    add google translate element

--- resources/fslaf-other.js
+++ resources/fslaf-other.js
@@ -5,5 +5,36 @@
 		$('#right-navigation').toggle( false );
 		$('.mw-editsection').toggle( false );
 	}
-	// $('.mw-logo').replaceWith($('#google_translate_element'));
+
+	// temporary, remove after updating to a recent MW version
+	$('header.mw-body-header').addClass('vector-page-titlebar');
+
+	// Create wrapper and container, append after first heading
+	var wrapper = $('<div id="p-lang-btn">').addClass(
+		'vector-dropdown mw-portlet mw-portlet-lang',
+	);
+	var container = $('<div>', { id: 'google_translate_element' });
+	wrapper.append(container);
+	$('#firstHeading').after(wrapper);
+
+	// Define the callback for Google Translate
+	window.googleTranslateElementInit = function () {
+		new google.translate.TranslateElement(
+			{
+				pageLanguage: mw.config.get('wgGoogleTranslatorOriginal'),
+				includedLanguages: mw.config.get('wgGoogleTranslatorLanguages'),
+				layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+			},
+			'google_translate_element',
+		);
+	};
+
+	// load and append the script, it cannot be added server-side
+	// since "p-lang-btn" must exist in the DOM when the callback
+	// is executed
+	var gtScript = document.createElement('script');
+	gtScript.type = 'text/javascript';
+	gtScript.src =
+		'//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+	document.head.appendChild(gtScript);
 } )();
