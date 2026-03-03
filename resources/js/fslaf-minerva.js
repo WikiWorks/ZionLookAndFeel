da2b5086171ee5d2cf36025f59f7d1b9d477b1ec
thomas-topway-it <topway.it@mail.com>
2026-03-04 03:36:08 +0400

    fix search bar

--- resources/js/fslaf-minerva.js
+++ resources/js/fslaf-minerva.js
@@ -1,5 +1,17 @@
-( function() {
+(function () {
 	if (mw.config.get('wgUserName') === null) {
-		$('.page-actions-menu').toggle( false );
+		$('.page-actions-menu').toggle(false);
 	}
-} )();
+
+	// fix search bar
+	mw.loader.using('mediawiki.router').then(function () {
+		const router = require('mediawiki.router');
+		router.on('route', function (route) {
+			if (route.path === '/search') {
+				$('body').css('overflow', 'hidden');
+			} else if (!route.path) {
+				$('body').css('overflow', 'visible');
+			}
+		});
+	});
+})();
